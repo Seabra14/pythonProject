ef7984fcfc187587ba627d0007caf0e147e59cdd
Seabra14 <88586353+Seabra14@users.noreply.github.com>
2021-08-20 18:49:15 +0100

    +++

---
+++ 0011.py
@@ -0,0 +1,7 @@
+larg = float(input("Largura da parede: "))
+alt = float(input("Altura da parede: "))
+área = larg * alt
+print("Sua parede tem uma dimensão de {}x{} e a sua área é de {}m2.".format(larg, alt, área))
+tinta = área / 2
+print ("Para pintar a parede, você precisará de {}l de tinta.".format(tinta))
+
---
+++ 012.py
@@ -0,0 +1,3 @@
+preço=float(input("Qual é o preço do produto? E"))
+novo=preço-(preço*5/100)
+print("O produto que custava {},na promoção com desconto de 5º/º vai custar {}.".format(preço,novo))
